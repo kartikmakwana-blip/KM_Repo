1dcc0a3455a99c3c34a6a1b4fd56acf8e58d2013
Kartik Cotestertest <kartik.makwana+1@testgrid.io>
2026-08-19 08:18:42 +0000

    Initial Version Commit

---
+++ compile-java-executable/1222/SuitePrim.java
@@ -0,0 +1,23 @@
+@Listeners(TestListener.class)
+public class SuitePrim {
+
+    public static String var_ra_runtimeurunapp = "ABC";
+
+    public static String var_nestapp = "com.nestdesign.nestforms";
+
+    public static String var_urunapp = "com.app.urunner";
+
+    public static int var_ra_rintdate = 12;
+
+    public static int var_GBInt = 100;
+
+    public static String var_GBStr = "GlobalString";
+
+    public static int var_gint = 2000;
+
+    public static String var_dateutility = "dateutility";
+
+    public static String var_ra_ra_str = "EFGH";
+
+    public static String var_gstr = "ABCD";
+}
