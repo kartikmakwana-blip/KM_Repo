2affe12abf539f7323f3e866cf555f893dcc6aa1
Kartik Cotestertest <kartik.makwana+1@testgrid.io>
2026-08-26 06:32:20 +0000

    FCopyComm

---
+++ compile-java-executable/1235/testcases/TC01.java
@@ -0,0 +1,37 @@
+import abc;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tc01 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tc01() {
+		tg.openDevice();
+				tg.swipe(Direction.DOWN);
+				tg.wait("ele_WalkStepRunandEarnTextView1787579857654", ComparisonType.IS_VISIBLE);
+				tg.click("ele_WalkStepRunandEarnTextView1787579857654", 1);
+				tg.wait("ele_EnteremailaddressEditText1787579866238", ComparisonType.IS_VISIBLE);
+				tg.click("ele_EnteremailaddressEditText1787579866238", 1);
+				tg.wait("ele_EnterpasswordEditText1787579874825", ComparisonType.IS_VISIBLE);
+				tg.click("ele_EnterpasswordEditText1787579874825", 1);
+				tg.wait("ele_EnterpasswordEditText1787579874825", ComparisonType.IS_VISIBLE);
+				tg.type("ele_EnterpasswordEditText1787579874825", "passs", true);
+				tg.wait("ele_textinputendiconImageButton1787579913089", ComparisonType.IS_VISIBLE);
+		tg.close();
+	}
+}
