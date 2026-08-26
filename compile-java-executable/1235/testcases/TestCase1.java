c6bad0d0b84568dfd17fc493983ce7af2d04d5af
Kartik Cotestertest <kartik.makwana+1@testgrid.io>
2026-08-26 08:02:41 +0000

    API Req

--- compile-java-executable/1235/testcases/TestCase1.java
+++ compile-java-executable/1235/testcases/TestCase1.java
@@ -22,16 +22,19 @@ public class testcase1 {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void testcase1() {
 		tg.openDevice();
-				tg.swipe(Direction.DOWN);
-				tg.wait("ele_WalkStepRunandEarnTextView1787579857654", ComparisonType.IS_VISIBLE);
-				tg.click("ele_WalkStepRunandEarnTextView1787579857654", 1);
-				tg.wait("ele_EnteremailaddressEditText1787579866238", ComparisonType.IS_VISIBLE);
-				tg.click("ele_EnteremailaddressEditText1787579866238", 1);
-				tg.wait("ele_EnterpasswordEditText1787579874825", ComparisonType.IS_VISIBLE);
-				tg.click("ele_EnterpasswordEditText1787579874825", 1);
-				tg.wait("ele_EnterpasswordEditText1787579874825", ComparisonType.IS_VISIBLE);
-				tg.type("ele_EnterpasswordEditText1787579874825", "passs", true);
-				tg.wait("ele_textinputendiconImageButton1787579913089", ComparisonType.IS_VISIBLE);
+		tg.swipe(Direction.DOWN);
+		tg.wait("ele_WalkStepRunandEarnTextView1787579857654", ComparisonType.IS_VISIBLE);
+		tg.click("ele_WalkStepRunandEarnTextView1787579857654", 1);
+		tg.wait("ele_EnteremailaddressEditText1787579866238", ComparisonType.IS_VISIBLE);
+		tg.click("ele_EnteremailaddressEditText1787579866238", 1);
+		tg.wait("ele_EnterpasswordEditText1787579874825", ComparisonType.IS_VISIBLE);
+		tg.click("ele_EnterpasswordEditText1787579874825", 1);
+		tg.wait("ele_EnterpasswordEditText1787579874825", ComparisonType.IS_VISIBLE);
+		tg.type("ele_EnterpasswordEditText1787579874825", "passs", true);
+		tg.wait("ele_textinputendiconImageButton1787579913089", ComparisonType.IS_VISIBLE);
+		tg_String var_v1 = "Null";
+				var_v1 = tg.readFromAPI("Req_api.json").getString();
+		tg.printLogs(var_v1);
 		tg.close();
 	}
 }
---
+++ compile-java-executable/constants.java
@@ -0,0 +1,4 @@
+public class constants {
+	public static int var_Gbstr = 222;
+	public static String var_ra_RnStr = "RunVariable";
+}
