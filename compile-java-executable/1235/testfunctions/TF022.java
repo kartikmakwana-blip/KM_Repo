ab61c1044eb32d4d98e872e7d614d2fab2436af7
Kartik Cotestertest <kartik.makwana+1@testgrid.io>
2026-08-26 10:31:20 +0000

    Commit

---
+++ compile-java-executable/1235/testfunctions/TF022.java
@@ -0,0 +1,24 @@
+import abc;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+class tf022 {
+
+	public static void tf022() {
+		tg.wait(1);
+	}
+}
