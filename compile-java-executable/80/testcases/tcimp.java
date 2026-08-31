b347aadc716e1a229c24508b9ea305b3c5407185
Kartik CAdmin <km.test+1@testgrid.io>
2026-08-31 15:20:47 +0000

    Initial Version Commit

---
+++ compile-java-executable/80/testcases/tcimp.java
@@ -0,0 +1,35 @@
+import com.mongodb.*;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tcimp {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tcimp() {
+		tg.openDevice();
+		tg.wait(1);
+		tg.close();
+	}
+}
