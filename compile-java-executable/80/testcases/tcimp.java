8addfdbaeedf6d5deab1e752bf989899a3948789
Kartik CAdmin <km.test+1@testgrid.io>
2026-08-31 15:21:15 +0000

    ImpTC

--- compile-java-executable/80/testcases/tcimp.java
+++ compile-java-executable/80/testcases/tcimp.java
@@ -1,11 +1,11 @@
-import com.mongodb.*;
-import org.bson.BsonDocument;
-import org.bson.BsonInt64;
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import org.bson.conversions.Bson;
+import org.bson.Document;
+import org.bson.BsonInt64;
+import org.bson.BsonDocument;
+import com.mongodb.*;
 import io.testgrid.listeners.TestListener;
 import io.testgrid.listeners.RetryFailedTestCases;
 import io.testgrid.tg;
@@ -29,7 +29,33 @@ public class tcimp {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void tcimp() {
 		tg.openDevice();
-		tg.wait(1);
+		START_CUSTOM_SCRIPT;
+		// Replace the placeholder with your Atlas connection string
+		        String uri = "mongodb+srv://yogeshappvolution:tqdlativpwmifhbl@testgrid.cz7ueay.mongodb.net/?retryWrites=true&w=majority&appName=testgrid";
+		
+		        // Construct a ServerApi instance using the ServerApi.builder() method
+		        ServerApi serverApi = ServerApi.builder()
+		                .version(ServerApiVersion.V1)
+		                .build();
+		
+		        MongoClientSettings settings = MongoClientSettings.builder()
+		                .applyConnectionString(new ConnectionString(uri))
+		                .serverApi(serverApi)
+		                .build();
+		
+		        // Create a new client and connect to the server
+		        try (MongoClient mongoClient = MongoClients.create(settings)) {
+		            MongoDatabase database = mongoClient.getDatabase("admin");
+		            try {
+		                // Send a ping to confirm a successful connection
+		                Bson command = new BsonDocument("ping", new BsonInt64(1));
+		                Document commandResult = database.runCommand(command);
+		                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+		            } catch (MongoException me) {
+		                System.err.println(me);
+		            }
+		        }
+		END_CUSTOM_SCRIPT;
 		tg.close();
 	}
 }
