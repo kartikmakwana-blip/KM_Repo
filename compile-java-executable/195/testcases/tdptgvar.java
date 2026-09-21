45e88fc47e557ff7155cc9edf28232c6e57e5cfe
Dhruvin vasoya <dhruvin.vasoya+23@testgrid.io>
2026-09-21 13:36:35 +0000

    gjkgt

--- compile-java-executable/195/testcases/tdptgvar.java
+++ compile-java-executable/195/testcases/tdptgvar.java
@@ -28,20 +28,20 @@ public class tdptgvar {
 		// [DISABLED] tg.wait("ele_EnterpasswordEditText1782830323675", ComparisonType.IS_VISIBLE);
 		// [DISABLED] tg.type("ele_EnterpasswordEditText1782830323675", "#TGITVAR.salary", false);
 		// [DISABLED] tg.wait(1);
-		tg_String var_empid = "#TGITVAR.employee_id";
-		tg_String var_fullname = "#TGITVAR.full_name";
-		tg_String var_email = "#TGITVAR.email";
-		tg_String var_deptname = "#TGITVAR.department_name";
-		tg_String var_salary = "#TGITVAR.salary";
-		tg_String var_status = "#TGITVAR.status";
-		tg_String var_date = "#TGITVAR.joined_on";
+		tg_String var_empid = "#TGITVAR.full_name";
+		tg_String var_fullname = "#TGITVAR.salary";
+		// [DISABLED] tg_String var_email = "#TGITVAR.email";
+		// [DISABLED] tg_String var_deptname = "#TGITVAR.department_name";
+		// [DISABLED] tg_String var_salary = "#TGITVAR.salary";
+		// [DISABLED] tg_String var_status = "#TGITVAR.status";
+		// [DISABLED] tg_String var_date = "#TGITVAR.joined_on";
 		tg.printLogs(var_empid);
 		tg.printLogs(var_fullname);
-		tg.printLogs(var_email);
-		tg.printLogs(var_deptname);
-		tg.printLogs(var_salary);
-		tg.printLogs(var_status);
-		tg.printLogs(var_date);
+		// [DISABLED] tg.printLogs(var_email);
+		// [DISABLED] tg.printLogs(var_deptname);
+		// [DISABLED] tg.printLogs(var_salary);
+		// [DISABLED] tg.printLogs(var_status);
+		// [DISABLED] tg.printLogs(var_date);
 		tg.close();
 	}
 }
